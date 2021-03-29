d38d30ee49c982927cb867d7bcf464b1bb029d46
Alex Power <alexanderpower1@outlook.com>
2021-03-29 18:53:51 +0100

    inital commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## Local Development
+
+To run the app locally, clone the repository and run the following commands:
+
+```
+npm i
+npm run start
+```
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "app.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: "./src/index.html", inject: "body" }),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    hot: true,
+    port: 8000,
+  },
+};
