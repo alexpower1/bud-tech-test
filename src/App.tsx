c59678d69172657e3693b52ef394ac5dd2a3a47d
Alex Power <alexanderpower1@outlook.com>
2021-03-29 21:43:05 +0100

    add basic styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { fetchData } from "./services/fetch-data-service";
+import styled, { ThemeProvider } from "styled-components";
+
 import { Statement } from "../types";
-import { ThemeProvider } from "styled-components";
+import { fetchData } from "./services/fetch-data-service";
 import theme from "./theme";
 
 const App: React.FC = () => {
@@ -26,37 +27,56 @@ const App: React.FC = () => {
     fetchBankStatementData();
   }, []);
 
+  const Wrapper = styled.div`
+    margin: 0 auto;
+    max-width: 1200px;
+  `;
+
+  const PrimaryHeading = styled.h1`
+    font-family: ${(props) => props.theme.font};
+  `;
+
+  const TextDetail = styled.p`
+    font-family: ${(props) => props.theme.font};
+  `;
+
   return (
     <div>
       {loading && <h1>Loading</h1>}
 
       {data && (
         <ThemeProvider theme={theme}>
-          <div>
-            <h1>{data.provider.title}</h1>
-            <p>Account Number: {data.provider.account_number}</p>
-            <p>Sort Code: {data.provider.sort_code}</p>
-            <p>{data.provider.description}</p>
-          </div>
+          <Wrapper>
+            <div>
+              <PrimaryHeading>
+                Hello, welcome to {data.provider.title}
+              </PrimaryHeading>
+              <TextDetail>
+                Account Number: {data.provider.account_number}
+              </TextDetail>
+              <TextDetail>Sort Code: {data.provider.sort_code}</TextDetail>
+              <TextDetail>{data.provider.description}</TextDetail>
+            </div>
 
-          <div>
-            <p>
-              Balance: {data.balance.currency_iso} {data.balance.amount}
-            </p>
-          </div>
+            <div>
+              <TextDetail>
+                Balance: {data.balance.currency_iso} {data.balance.amount}
+              </TextDetail>
+            </div>
 
-          {data.transactions.map((transaction) => {
-            return (
-              <div>
-                <p>{transaction.date}</p>
-                <p>{transaction.description}</p>
-                <p>{transaction.category_title}</p>
-                <p>
-                  {transaction.amount.currency_iso} {transaction.amount.value}
-                </p>
-              </div>
-            );
-          })}
+            {data.transactions.map((transaction) => {
+              return (
+                <div key={transaction.id}>
+                  <TextDetail>{transaction.date}</TextDetail>
+                  <TextDetail>{transaction.description}</TextDetail>
+                  <TextDetail>{transaction.category_title}</TextDetail>
+                  <TextDetail>
+                    {transaction.amount.currency_iso} {transaction.amount.value}
+                  </TextDetail>
+                </div>
+              );
+            })}
+          </Wrapper>
         </ThemeProvider>
       )}
 
