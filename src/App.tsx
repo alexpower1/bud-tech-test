05d57cde08d7d456db9e57592560e03ffee5e9f8
Alex Power <alexanderpower1@outlook.com>
2021-03-29 20:57:07 +0100

    display data in basic list

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { fetchData } from "./services/fetch-data-service";
+import { Statement } from "../types";
+import { ThemeProvider } from "styled-components";
+import theme from "./theme";
 
-const App = () => {
-  const [data, setData] = useState();
+const App: React.FC = () => {
+  const [data, setData] = useState<Statement>();
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    setLoading(true);
-
+  const fetchBankStatementData = () => {
+    setLoading(false);
     fetchData()
       .then((data) => {
         setData(data);
@@ -18,16 +20,44 @@ const App = () => {
         setError(error);
         setLoading(false);
       });
+  };
+
+  useEffect(() => {
+    fetchBankStatementData();
   }, []);
 
   return (
     <div>
       {loading && <h1>Loading</h1>}
+
       {data && (
-        <>
-          <h1>Data</h1>
-          <pre>{JSON.stringify(data, null, 2)}</pre>
-        </>
+        <ThemeProvider theme={theme}>
+          <div>
+            <h1>{data.provider.title}</h1>
+            <p>Account Number: {data.provider.account_number}</p>
+            <p>Sort Code: {data.provider.sort_code}</p>
+            <p>{data.provider.description}</p>
+          </div>
+
+          <div>
+            <p>
+              Balance: {data.balance.currency_iso} {data.balance.amount}
+            </p>
+          </div>
+
+          {data.transactions.map((transaction) => {
+            return (
+              <div>
+                <p>{transaction.date}</p>
+                <p>{transaction.description}</p>
+                <p>{transaction.category_title}</p>
+                <p>
+                  {transaction.amount.currency_iso} {transaction.amount.value}
+                </p>
+              </div>
+            );
+          })}
+        </ThemeProvider>
       )}
 
       {error && (
