d1efdc35d476473c0597b4bd83a76b81b8c76b5a
Alex Power <alexanderpower1@outlook.com>
2021-03-29 20:25:15 +0100

    add type declarations

---
+++ types.d.ts
@@ -0,0 +1,35 @@
+export enum CurrencyISO {
+  Gbp = "GBP",
+}
+
+export interface Statement {
+  id: string;
+  provider: Provider;
+  balance: Balance;
+  transactions: Transaction[];
+}
+
+export interface Balance {
+  amount: number;
+  currency_iso: CurrencyISO;
+}
+
+export interface Provider {
+  title: string;
+  account_number: string;
+  sort_code: string;
+  description: string;
+}
+
+export interface Transaction {
+  id: string;
+  date: Date;
+  description: string;
+  category_title: string;
+  amount: Amount;
+}
+
+export interface Amount {
+  value: number;
+  currency_iso: CurrencyISO;
+}
